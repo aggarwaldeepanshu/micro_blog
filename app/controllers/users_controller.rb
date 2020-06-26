b2e36a2b764335773069ef2d8d077e6cb773697c
Swati <swati@Deepanshus-MacBook-Air.local>
2020-06-26 17:09:00 +0530

    remove space in users controller create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,7 +17,6 @@ class UsersController < ApplicationController
 
   def create
   	@user = User.new(user_params)
-
   	if(@user.save)
       log_in @user
   		flash[:success] = "Welcome to the MicroBlog" + " #{@user.name}\n"
